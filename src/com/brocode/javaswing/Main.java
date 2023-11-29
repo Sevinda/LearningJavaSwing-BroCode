72de68236afcc59a8cfb06227aa9b1f595a6342e
Sevinda Perera <sevindacharukaperera@gmail.com>
2023-11-29 13:59:26 +0530

    frames

---
+++ src/com/brocode/javaswing/Main.java
@@ -0,0 +1,14 @@
+package com.brocode.javaswing;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setSize(420, 420);
+        frame.setTitle("Java Swing Bro Code");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+    }
+}
